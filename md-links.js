41bfa51b565f72ae43527abdd2decfe1c9ad1d37
cynthiagomezmontoya <cgomezm066@gmail.com>
2019-09-02 06:34:54 -0400

    leer archivo md

--- md-links.js
+++ md-links.js
@@ -21,3 +21,14 @@ const readingPath = (path =>{
   })
 })
 
+//leer archivo.md
+const read = (path => {
+    return new Promise((resolve,reject)=>{
+      fs.readFile( path,'utf8', (err, data) => {
+        if (err){
+          reject(err("archivo no encontrado"+path))
+        }
+        resolve(data)
+      })
+    })
+  })
